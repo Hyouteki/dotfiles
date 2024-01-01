470f53531fbd6e8576f9b986f372ec8b5ed78c50
Lakshay <lakshay21060@iiitd.ac.in>
2024-01-01 16:45:01 +0530

    changed location of backups

--- init.el
+++ init.el
@@ -58,9 +58,18 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; ido-mode
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
+;; resizing minibuffer
 (setq resize-mini-windows  t)
 
+;; write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
