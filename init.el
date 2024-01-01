d9dd643e01acb01d6ead4a027f1a849419442471
Lakshay <lakshay21060@iiitd.ac.in>
2024-01-01 17:32:07 +0530

    added rust-mode

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme use-package)))
+ '(package-selected-packages '(rust-mode zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -73,3 +73,6 @@
       delete-old-versions    t  ; Automatically delete excess backups:
       kept-new-versions      20 ; how many of the newest versions to keep
       kept-old-versions      5) ; and how many of the old
+
+;; language modes
+(require 'rust-mode)
