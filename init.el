7f963d50933e970946a6739ff4facf822fd92441
Lakshay <lakshay21060@iiitd.ac.in>
2024-01-01 14:43:19 +0530

    added ido mode in emacs config

--- init.el
+++ init.el
@@ -44,3 +44,20 @@
 
 ;; syntax highlighting
 (setq font-lock-maximum-decoration t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(zenburn-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq indo-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
